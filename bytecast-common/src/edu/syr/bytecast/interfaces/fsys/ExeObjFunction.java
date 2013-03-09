16de9a8efe799025de478e45a942ac9554d5c924
Shawn Mueller <smueller@syr.edu>
2013-03-09 16:02:41 -0500

    Added .equals and .hashCode functions to all EXeObj*.java files. Moved test back to bytecast-test

--- bytecast-common/src/edu/syr/bytecast/interfaces/fsys/ExeObjFunction.java
+++ bytecast-common/src/edu/syr/bytecast/interfaces/fsys/ExeObjFunction.java
@@ -35,4 +35,35 @@ public class ExeObjFunction {
     {
         return m_size;
     }
+
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 53 * hash + (this.m_name != null ? this.m_name.hashCode() : 0);
+        hash = 53 * hash + (int) (this.m_startAddress ^ (this.m_startAddress >>> 32));
+        hash = 53 * hash + this.m_size;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ExeObjFunction other = (ExeObjFunction) obj;
+        if ((this.m_name == null) ? (other.m_name != null) : !this.m_name.equals(other.m_name)) {
+            return false;
+        }
+        if (this.m_startAddress != other.m_startAddress) {
+            return false;
+        }
+        if (this.m_size != other.m_size) {
+            return false;
+        }
+        return true;
+    }
 }
--- bytecast-common/src/edu/syr/bytecast/interfaces/fsys/ExeObjSegment.java
+++ bytecast-common/src/edu/syr/bytecast/interfaces/fsys/ExeObjSegment.java
@@ -64,6 +64,38 @@ public class ExeObjSegment {
     {
         return m_startAddress + m_bytes.size();
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + (int) (this.m_startAddress ^ (this.m_startAddress >>> 32));
+        hash = 79 * hash + (this.m_label != null ? this.m_label.hashCode() : 0);
+        hash = 79 * hash + (this.m_bytes != null ? this.m_bytes.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ExeObjSegment other = (ExeObjSegment) obj;
+        if (this.m_startAddress != other.m_startAddress) {
+            return false;
+        }
+        if ((this.m_label == null) ? (other.m_label != null) : !this.m_label.equals(other.m_label)) {
+            return false;
+        }
+        if (this.m_bytes != other.m_bytes && (this.m_bytes == null || !this.m_bytes.equals(other.m_bytes))) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     private long m_startAddress;
     private String m_label;
     private List<Byte> m_bytes;
