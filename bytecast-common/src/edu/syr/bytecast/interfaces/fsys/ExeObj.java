7c30cd933769a4faca3545ef7bcf24794d723620
Shawn Mueller <smueller@syr.edu>
2013-03-09 09:56:58 -0500

    Fixed bug in bounds checking for getBytesFromAddr

--- bytecast-common/src/edu/syr/bytecast/interfaces/fsys/ExeObj.java
+++ bytecast-common/src/edu/syr/bytecast/interfaces/fsys/ExeObj.java
@@ -4,24 +4,28 @@ import java.io.*;
 
 public class ExeObj {
     
+    //Returns the entry point of the program of the program
     public long getEntryPointIndex() {
         return m_entryPointIndex;
     }
      
+    //Sets the entry point of the program
     public void setEntryPointIndex(long entry_point) {
         m_entryPointIndex = entry_point;
     }
     
+    //returns all memory segments
     public List<ExeObjSegment> getSegments() {
         return m_segments;
     }
     
+    //Returns a group of bytes from a virtual address
     public List<Byte> getBytesFromAddr(long start_addr, int size) throws Exception
     {
         for(int i = 0; i < m_segments.size(); i++)
         {
             if(start_addr >= m_segments.get(i).getStartAddress() &&
-               start_addr+size <=  m_segments.get(i).getEndAddress())
+               start_addr+size <  m_segments.get(i).getEndAddress())
             {
                 int offset = (int)(start_addr - m_segments.get(i).getStartAddress());
                 return m_segments.get(i).getBytes(offset,size);
@@ -30,14 +34,17 @@ public class ExeObj {
         throw new Exception("Address range not found in any segment.");
     }
     
+    //Sets all segments
     public void setSegments(List<ExeObjSegment> segments) {
         m_segments=segments;
     }
  
+    //Sets all functions
     public void setFunctions(List<ExeObjFunction> functions){
         m_functions = functions;
     }
     
+    //Returns all functions. 
     public List<ExeObjFunction> getFunctions()
     {
         return m_functions;        
