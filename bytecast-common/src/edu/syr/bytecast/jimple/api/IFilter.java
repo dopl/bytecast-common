10dd192fa5348841097f4b75ba47d88f6d11396e
Fei Qi <qifei1989@126.com>
2013-03-24 15:34:13 -0400

    change the api of jimple

--- bytecast-common/src/edu/syr/bytecast/jimple/api/IFilter.java
+++ bytecast-common/src/edu/syr/bytecast/jimple/api/IFilter.java
@@ -5,16 +5,16 @@
 package edu.syr.bytecast.jimple.api;
 
 import edu.syr.bytecast.amd64.api.instruction.IInstruction;
+import edu.syr.bytecast.amd64.api.output.MemoryInstructionPair;
 import edu.syr.bytecast.jimple.beans.ParsedInstructionsSet;
 import java.util.List;
-import java.util.Map;
 
 /**
  *
  * @author QSA
  */
 public interface IFilter{
-  boolean doTest(Map<Long, IInstruction> instList, int index);
+  boolean doTest(List<MemoryInstructionPair> instList, int index);
     //void scan(List<IInstruction> unparsed_inst_list, List<ParsedInstructionsSet> parsed_inst_list, List<IFilterRule> rulesList);
     
 }
